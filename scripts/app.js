e50c28bc38d83f7825ced07d010db5f103fb3b53
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2022-11-12 16:43:59 +0100

    Finished: snake array, setInterval, controls, enhanced random apple (need testing), direction of travel

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@ function initiation() {
   const startButton = document.querySelector(".start-button");
   const grid = document.querySelector(".grid");
   const score = document.querySelector(".score");
+  // const allCellTest = document.querySelectorAll(".cell");
+  // const cellTest = allCellTest.
 
   // ! Grid functions
 
@@ -13,7 +15,7 @@ function initiation() {
     for (let index = 0; index < gridCellCount; index++) {
       const cell = document.createElement("div");
       cell.setAttribute("data-index", index);
-      // cell.setAttribute("class", "cell");
+      cell.setAttribute("class", "cell");
       cells.push(cell);
       grid.appendChild(cell);
     }
@@ -25,6 +27,8 @@ function initiation() {
     // ! APPLE FUNCTIONS
 
     let applePosition = 0;
+    let snake = [3, 2, 1, 0];
+    let directionOfTravel = "right";
 
     function addApple(position) {
       cells[position].classList.add("apple");
@@ -34,23 +38,29 @@ function initiation() {
       cells[position].classList.remove("apple");
     }
 
+    // function getRandomPosition() {
+    //   return Math.floor(Math.random() * gridCellCount);
+    // }
+
     function getRandomPosition() {
-      return Math.floor(Math.random() * gridCellCount);
+      let randomPosition = Math.floor(Math.random() * gridCellCount);
+      while (snake.includes(randomPosition)) {
+        randomPosition = Math.floor(Math.random() * gridCellCount);
+        // return randomPosition;
+      }
+      return randomPosition;
     }
 
     function spawnApple() {
       removeApple(applePosition);
       applePosition = getRandomPosition();
       addApple(applePosition);
-      console.log(applePosition);
     }
     spawnApple();
 
     // ! SNAKE FUNCTIONS
 
     // ? SPAWN SNAKE
-
-    let snake = [3, 2, 1, 0];
 
     function renderSnake() {
       snake.forEach((cell) => cells[cell].classList.add("snake"));
@@ -62,53 +72,91 @@ function initiation() {
       snake.forEach((cell) => cells[cell].classList.remove("snake"));
     }
 
-    // ? MOVING THE SNAKE
-
-    let directionOfTravel;
+    // ? GET DIRECTION OF TRAVEL
 
     function getDirectionOfTravel(event) {
-      if (event.key === "ArrowRight") {
-        directionOfTravel = "right";
-        console.log(directionOfTravel);
-      } else if (event.key === "ArrowDown") {
-        directionOfTravel = "down";
-        console.log(directionOfTravel);
-      } else if (event.key === "ArrowLeft") {
-        directionOfTravel = "left";
-        console.log(directionOfTravel);
-      } else if (event.key === "ArrowUp") {
-        directionOfTravel = "up";
-        console.log(directionOfTravel);
+      switch (event.keyCode) {
+        case 39:
+          directionOfTravel = "right";
+          break;
+        case 40:
+          directionOfTravel = "down";
+          break;
+        case 37:
+          directionOfTravel = "left";
+          break;
+        case 38:
+          directionOfTravel = "up";
       }
     }
 
     window.addEventListener("keydown", getDirectionOfTravel);
 
-    function moveSnake(event) {
+    // ? MOVING THE SNAKE
+
+    function moveRight() {
+      snake.pop();
+      snake.unshift(snake[0] + 1);
+    }
+
+    function moveLeft() {
+      snake.pop();
+      snake.unshift(snake[0] - 1);
+    }
+
+    function moveUp() {
+      snake.pop();
+      snake.unshift(snake[0] - 10);
+    }
+
+    function moveDown() {
+      snake.pop();
+      snake.unshift(snake[0] + 10);
+    }
+
+    function moveSnake() {
+      setInterval(() => {
+        const x = snake[0] % width;
+        const y = Math.floor(snake[0] / width);
+        removeSnake();
+        if (directionOfTravel === "right" && x < width - 1) {
+          moveRight();
+        } else if (directionOfTravel === "down" && y < width - 1) {
+          moveDown();
+        } else if (directionOfTravel === "left" && x > 0) {
+          moveLeft();
+        } else if (directionOfTravel === "up" && y > 0) {
+          moveUp();
+        }
+        renderSnake();
+      }, 300);
+    }
+
+    moveSnake();
+
+    // ? CONTROLLING THE SNAKE
+
+    function controlSnake(event) {
       removeSnake();
       const x = snake[0] % width;
       const y = Math.floor(snake[0] / width);
-      if (event.key === "ArrowRight" && x < width - 1) {
-        snake.pop();
-        snake.unshift(snake[0] + 1);
-      } else if (event.key === "ArrowDown" && y < width - 1) {
-        snake.pop();
-        snake.unshift(snake[0] + 10);
-      } else if (event.key === "ArrowLeft" && x > 0) {
-        snake.pop();
-        snake.unshift(snake[0] - 1);
-      } else if (event.key === "ArrowUp" && y > 0) {
-        snake.pop();
-        snake.unshift(snake[0] - 10);
+      if (event.keyCode === 39 && x < width - 1) {
+        moveRight();
+      } else if (event.keyCode === 40 && y < width - 1) {
+        moveDown();
+      } else if (event.keyCode === 37 && x > 0) {
+        moveLeft();
+      } else if (event.keyCode === 38 && y > 0) {
+        moveUp();
       }
       renderSnake();
     }
 
-    window.addEventListener("keydown", moveSnake);
+    window.addEventListener("keydown", controlSnake);
 
     // ? EATING THE APPLE
 
-    // ? Score function //////////////////////////////////////
+    // ! SCORE FUNCTIONS
 
     let currentScore = 0;
 
@@ -117,13 +165,10 @@ function initiation() {
       score.innerHTML = currentScore;
     }
 
-    // ? Snake eats apple ////////////////////////////////////
+    // ! END GAME FUNCTIONS
   }
-  // Start game functions ////////////////////////////
 
   startButton.addEventListener("click", startGame);
-
-  // ! END GAME FUNCTIONS
 }
 
 window.addEventListener("DOMContentLoaded", initiation);
